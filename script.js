f221e0e29a47635199585a05b049acea17444fcd
Siddhant Pasari <Siddhant.Pasari@paconsulting.com>
2021-07-27 10:36:48 -0400

    Added arrows and slider dots

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
     let imageList = [
-        '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg',
+        '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg',
     ]
     createSliderBody(imageList, loadSliderWithData)
 })
 
 
-let createSliderBody = function(imageList, callback){
+let createSliderBody = function (imageList, callback) {
     let sliderContainer = document.getElementById('my-keen-slider');
     let slideLen = imageList.length;
-    for(let i=0; i<slideLen; i++){
+    for (let i = 0; i < slideLen; i++) {
         let slide = document.createElement('div');
-        slide.classList.add('keen-slider__slide','lazy__slide');
+        slide.classList.add('keen-slider__slide', 'lazy__slide');
         let slideImg = document.createElement('div');
         slide.appendChild(slideImg)
         sliderContainer.appendChild(slide)
@@ -19,10 +19,36 @@ let createSliderBody = function(imageList, callback){
     callback(imageList);
 }
 
-let loadSliderWithData = function (images){
+let loadSliderWithData = function (images) {
     let elements = document.querySelectorAll(".lazy__slide div")
     let loaded = [];
     let slider = new KeenSlider("#my-keen-slider", {
+        created: function (instance) {
+            document
+                .getElementById("arrow-left")
+                .addEventListener("click", function () {
+                    instance.prev()
+                })
+            document
+                .getElementById("arrow-right")
+                .addEventListener("click", function () {
+                    instance.next()
+                })
+            let dots_wrapper = document.getElementById("dots")
+            let slides = document.querySelectorAll(".keen-slider__slide")
+            slides.forEach(function (t, idx) {
+                let dot = document.createElement("button")
+                dot.classList.add("dot")
+                dots_wrapper.appendChild(dot)
+                dot.addEventListener("click", function () {
+                    instance.moveToSlide(idx)
+                })
+            })
+            updateClasses(instance)
+        },
+        slideChanged(instance) {
+            updateClasses(instance)
+        },
         afterChange: (s) => {
             const slideIdx = s.details().relativeSlide
             loaded[slideIdx] = true
@@ -32,5 +58,24 @@ let loadSliderWithData = function (images){
         },
         loop: true,
         initial: 0,
+    })
+}
+
+let updateClasses = function (instance) {
+    let slide = instance.details().relativeSlide;
+    let arrowLeft = document.getElementById("arrow-left")
+    let arrowRight = document.getElementById("arrow-right")
+    slide === 0
+        ? arrowLeft.classList.add("arrow--disabled")
+        : arrowLeft.classList.remove("arrow--disabled")
+    slide === instance.details().size - 1
+        ? arrowRight.classList.add("arrow--disabled")
+        : arrowRight.classList.remove("arrow--disabled")
+
+    let dots = document.querySelectorAll(".dot")
+    dots.forEach(function (dot, idx) {
+        idx === slide
+            ? dot.classList.add("dot--active")
+            : dot.classList.remove("dot--active")
     })
 }
